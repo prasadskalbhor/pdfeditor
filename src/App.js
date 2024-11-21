8f3358fa181347aa0db379721547464d532d3e0a
prasadskalbhor <prasad@setoo.co>
2024-11-21 12:55:13 +0530

    added pdf edit code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PdfViewer from './PDFViewer';
 
 const App = () => {
-  return<> <h1>This is pdf emdeding</h1><PdfViewer pdfUrl="/story1.pdf" /></>;
+  return<> <h1>This is pdf emdeding</h1><PdfViewer pdfUrl="/mypdf.pdf" /></>;
 };
 
 export default App;
