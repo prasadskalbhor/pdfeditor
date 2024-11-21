0707588640269a5e8ce7b8ce2ae11a03a51b63a4
prasadskalbhor <prasad@setoo.co>
2024-11-21 15:32:00 +0530

    remove form index js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-// import PDFViewer from './PDFViewer';
+import PDFViewer from './PDFViewer';
 
 const App = () => {
-  return<> <h1>This is pdf emdeding</h1>
-  {/* <PDFViewer pdfUrl="/mypdf.pdf" /> */}
+  return<> <h1>This is pdf embeding</h1>
+  <PDFViewer pdfUrl="/mypdf.pdf" />
   </>;
 };
 
