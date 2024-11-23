c07cc75509caa76ce9b0fb3924b593fdf1f1c0c4
Prasad Kalbhor <124653416+prasadskalbhor@users.noreply.github.com>
2024-11-23 18:58:57 +0530

    blob

--- src/PDFViewer.js
+++ src/PDFViewer.js
@@ -1,5 +1,31 @@
- import React, { useEffect, useState } from "react";
+  import React, { useEffect, useState } from "react";
+import { base64 } from "./base64";
 
+
+
+const byteToBlob = () => {
+  const base64str = base64 // decode base64 string, remove space for IE compatibility
+  console.log("blob ")
+
+  var binary = atob(base64str.replace(/\s/g, ''));
+  var len = binary.length;
+  console.log("blob 2")
+  var buffer = new ArrayBuffer(len);
+  console.log("blob 3")
+  var view = new Uint8Array(buffer);
+  console.log("blob 4")
+  for (var i = 0; i < len; i++) {
+    view[i] = binary.charCodeAt(i);
+  }
+  console.log("blob 5")
+
+  // create the blob object with content-type "application/pdf"               
+  var blob = new Blob([view], { type: "application/pdf" });
+  console.log("blob 6")
+  var url = URL.createObjectURL(blob);
+  console.log("this is url ::: ", url)
+  return blob;
+}
 const PDFBase64Viewer = () => {
   const urlToPDF = "/mypdf.pdf";
   const clientId = "e45ea6964465450fbc12e9a8329542d4";
@@ -11,8 +37,8 @@ const PDFBase64Viewer = () => {
     showDownloadPDF: false,
     showLeftHandPanel: false,
     showAnnotationTools: false,
-    showThumbnails:false,
-    dockPageControls: false, 
+    showThumbnails: false,
+    dockPageControls: false,
 
   };
   const saveOptions = {
@@ -68,12 +94,14 @@ const PDFBase64Viewer = () => {
         },
         saveOptions
       );
-
+      console.log("converting to blob:: 1")
+      const pdfBlob = byteToBlob();
+      console.log("converting to blob:: 2")
       // Load PDF into Viewer
       adobeDCView.previewFile(
         {
           content: {
-            location: { url: urlToPDF },
+            promise: Promise.resolve(pdfBlob), // Provide the Blob as a Promise
           },
           metaData: { fileName: "Check_Request_Full.pdf" },
         },
@@ -95,27 +123,27 @@ const PDFBase64Viewer = () => {
 
   return (
     <div style={{ width: "100%", height: "100vh", display: "flex", flexDirection: "column" }}>
-      <div id="embeddedView" style={{ flexGrow: 1}}></div>
-   <button
-          onClick={handleSubmitClick}
-          style={{
-            backgroundColor: "darkred",
-            height: "100%",
-            padding: "0 20px",
-            borderRadius: "5px",
-            color: "white",
-            fontWeight: "bold",
-            marginRight: "6px",
-            border: "1px solid white",
-            maxHeight: "50px",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            cursor: "pointer",
-          }}
-        >
-          Submit
-        </button>
+      <div id="embeddedView" style={{ flexGrow: 1, backgroundColor: 'red' }}></div>
+      <button
+        onClick={handleSubmitClick}
+        style={{
+          backgroundColor: "darkred",
+          height: "100%",
+          padding: "0 20px",
+          borderRadius: "5px",
+          color: "white",
+          fontWeight: "bold",
+          marginRight: "6px",
+          border: "1px solid white",
+          maxHeight: "50px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          cursor: "pointer",
+        }}
+      >
+        Submit
+      </button>
     </div>
   );
 };
